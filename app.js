2733be581ea7e6e3399a05c0a955396119098429
yosefkirkcohen <ycohen45@yahoo.com>
2021-08-18 14:46:24 -0700

    working game

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 // import functions and grab DOM elements
-
+import { getRandomThrow, didUserWin } from "./utils.js";
 // initialize global state
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+const playButton = document.getElementById('play-button');
+
+playButton.addEventListener('click', () => {
+  const userChoice = document.querySelector('input:checked');
+  const computerChoice = getRandomThrow();
+  const winOrLose = didUserWin(userChoice.value, computerChoice);
+  console.log(winOrLose);
+})
+
